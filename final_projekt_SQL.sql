dca8facd4a740ae52e87ac60827647d90ac230bc
Damian Ebner <damian.ebner@live.com>
2023-03-03 15:04:42 +0200

    Table + readme
    added first version of SQL primary table
    added description into readme

---
+++ final_projekt_SQL.sql
@@ -0,0 +1,25 @@
+/*
+ *final projekt sql tabulka 
+ */
+
+CREATE OR REPLACE TABLE t_damian_ebner_project_SQL_primary_final (
+ID INT AUTO_INCREMENT PRIMARY KEY,
+industry_branch_code char(1) charset utf16 DEFAULT NULL,
+industry_branch varchar(255) charset utf16 DEFAULT NULL,
+price_measure_from datetime NOT NULL,
+price_measure_to datetime NOT NULL,
+payroll_year int(11) DEFAULT NULL,
+payroll_quarter int(11) DEFAULT NULL,
+product_price int(4) DEFAULT NULL,
+p5roduct_id int(11) NOT NULL,
+product_category int(11) NOT NULl
+) ENGINE=InnoDB DEFAULT CHARSET=utf16
+; 
+
+
+
+
+
+
+
+
